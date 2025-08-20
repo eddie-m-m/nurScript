0d5776ea339de6d8f001b033c890445b78cbf516
eddie-m-m <eddie.m.menefee@gmail.com>
2025-08-20 11:35:17 -0700

    Add initial build system, tags

---
+++ build/Debug/CMakeFiles/nurScript.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nurScript.dir/src/main.cpp.o"
+  "CMakeFiles/nurScript.dir/src/main.cpp.o.d"
+  "nurScript"
+  "nurScript.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nurScript.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
